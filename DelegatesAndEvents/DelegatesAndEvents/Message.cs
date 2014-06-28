23f21799dbcc9ccabb1a7850ef39b5151f6e8607
Andrew Norton <andyjnorton@mac.com>
2014-06-28 13:33:32 +0100

    Initial

---
+++ DelegatesAndEvents/DelegatesAndEvents/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class Message 
+    {
+
+        public string MessageContent { get; set; }
+        public string Sender { get; set; }
+       
+        public MessageSentHandler msgHandler;
+
+        public void SendMessage()
+        {
+            if(msgHandler != null)
+            {
+                msgHandler(this.Sender, this.MessageContent);
+            }
+            
+        }
+
+
+    }
+}
---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Message msg = new Message();
+            msg.msgHandler = OnMessageReceived;
+            msg.msgHandler += LogMessage;
+
+            msg.Sender = "xxx";
+            msg.MessageContent = "message xxx";
+            msg.SendMessage();
+            
+            Console.ReadLine();
+        }
+
+        private static void OnMessageReceived(string messageFrom, string messageContent)
+        {
+            Console.WriteLine("Message sent to {0} : {1}", messageFrom, messageContent);
+          
+        }
+
+        private static void LogMessage(string messageFrom, string messageContent)
+        {
+            Console.WriteLine("Save message in db");
+           
+        }
+            
+    }
+}
